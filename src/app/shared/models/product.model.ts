57f6d41d7e5ec9367ee1a2b45d91c97e7aa350b4
Yaroslav Kovalov <yarik19022001@outlook.com>
2023-07-05 11:52:29 +0300

    added create category

--- src/app/shared/models/product.model.ts
+++ src/app/shared/models/product.model.ts
@@ -1,11 +1,10 @@
 export class Product{
-    constructor(public id: number
-        ,public name: string,
+    constructor(public id: number,
+        public name: string,
         public slug: string,
         public description: string,
         public categoryId: number,
         public price: number,
         public image: string){
-
     }
 }
